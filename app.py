cc65edeee1fc4d3b6fd88ce68377abab1e12a7f3
Turki55 <atrkl243@gmail.com>
2026-06-03 02:45:37 +0300

    fix port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 
@@ -72,4 +73,4 @@ def sell():
     return render_template('sell.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
